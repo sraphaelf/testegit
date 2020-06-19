4e7e3c27fc60c20d416d207e8441cc2da9f48b51
Raphael <sraphaelf@gmail.com>
2020-06-19 14:51:13 -0300

    Versionando pro server

--- TesteGit/TesteGit/Startup.cs
+++ TesteGit/TesteGit/Startup.cs
@@ -18,6 +18,7 @@ namespace TesteGit
             Configuration = configuration;
         }
 
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
